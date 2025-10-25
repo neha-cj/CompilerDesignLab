36283bb6ca313270f9af4780b4b6be9ca5efb7ff
Neha C J <119199146+neha-cj@users.noreply.github.com>
2025-10-25 19:46:38 +0530

    Update shift_reduce.c

--- cycle3/shift_reduce/shift_reduce.c
+++ cycle3/shift_reduce/shift_reduce.c
@@ -1,80 +1,68 @@
 #include <stdio.h>
 #include <string.h>
-// shift reducer parser
-// S -> S + S
-// S -> S - S
-// S -> (S)
-// S -> i
 
 char inp[100];
 int len;
 char stack[100];
-int top = 0;
+int top=0;
 
-void print_details(int ind, char *action)
-{
+void print_details(int ind, char *action){
     printf("$");
-    for (int i = 0; i <= top; i++)
+    for(int i=0;i<=top;i++){
         printf("%c", stack[i]);
+    }
     printf("\t\t");
-    for (int i = ind; i < len; i++)
+    for(int i=ind;i<len;i++){
         printf("%c", inp[i]);
+    }
     printf("$\t\t%s\n", action);
 }
 
-void check_for_reduce(int i)
-{
-    int flag = 1;
-    while (flag)
-    {
-        flag = 0;
-        if (stack[top - 2] == 'S' && stack[top - 1] == '+' && stack[top] == 'S')
-        {
-            print_details(i + 1, "REDUCE");
-            stack[top - 2] = 'S';
-            top = top - 2;
-            flag = 1;
+void check_for_reduce(int i){
+    int flag=1;
+    while(flag){
+        flag=0;
+        if(stack[top-2]=='S' && stack[top-1]=='+' && stack[top]=='S'){
+            print_details(i+1, "REDUCE");
+            stack[top-2]='S';
+            top=top-2;
+            flag=1;
         }
-        else if (stack[top - 2] == 'S' && stack[top - 1] == '-' && stack[top] == 'S')
-        {
-            print_details(i + 1, "REDUCE");
-            stack[top - 2] = 'S';
-            top = top - 2;
-            flag = 1;
+        else if(stack[top-2]=='S' && stack[top-1]=='-' && stack[top]=='S'){
+            print_details(i+1,"REDUCE");
+            stack[top-2]=='S';
+            top=top-2;
+            flag=1;
         }
-        else if (stack[top - 2] == '(' && stack[top - 1] == 'S' && stack[top] == ')')
-        {
-            print_details(i + 1, "REDUCE");
-            stack[top - 2] = 'S';
-            top = top - 2;
-            flag = 1;
+        else if(stack[top - 2] == '(' && stack[top - 1] == 'S' && stack[top] == ')'){
+            print_details(i+1, "REDUCE");
+            stack[top-2]='S';
+            top=top-2;
+            flag=1;
         }
-        else if (stack[top] == 'i')
-        {
-            print_details(i + 1, "REDUCE");
-            stack[top] = 'S';
-            flag = 1;
+        else if(stack[top]=='i'){
+            print_details(i+1, "REDUCE");
+            stack[top]='S';
+            flag=1;
         }
     }
 }
 
-int main()
-{
-    printf("Enter input:\n");
-    scanf("%s", inp);
-    len = strlen(inp);
+int main() {
+    printf("enter input:\n");
+    scanf("%s",inp);
+    len=strlen(inp);
     printf("Stack\t\tInput\t\tAction\n");
-    for (int i = 0; i < len; i++)
-    {
-        print_details(i, "SHIFT");
-        printf("stack = %s\n", stack);
-        stack[top] = inp[i];
+    for(int i=0;i<len;i++){
+        stack[top]= inp[i];
+        print_details(i,"SHIFT");
         check_for_reduce(i);
         top++;
     }
     top--;
-    if (top == 0 && stack[0] == 'S')
-        printf("Accepted.\n");
+    if(top==0 && stack[0]=='S')
+        printf("Accepeted.\n");
     else
-        printf("Rejected.\n");
+        printf("Rejected\n");
+    return 0;
 }
